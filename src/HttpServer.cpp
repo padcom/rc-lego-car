cacc3c86896e50c5fa5c822d5d7dee8cf96a3fc9
Matthias Hryniszak <padcom@gmail.com>
2018-11-13 01:09:23 +0100

    Code cleanup: split default handler into file and error handler

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -20,26 +20,34 @@ void HttpServer::registerHandler(String method, String path, PathHandler handler
   handlers.append(method + " " + path, handler);
 }
 
-void defaultErrorHandler(HttpRequest &request, HttpResponse &response) {
+void respondWithFile(String path, HttpResponse &response) {
+  byte buffer[256];
+  File f = SPIFFS.open(path, "r");
+  response.status(200, "FILE");
+  response.header("Content-Type", getMimeType(path));
+  response.header("Content-Size", String(f.size()));
+  response.beginBody();
+  while (f.position() < f.size()) {
+    int bytesRead = f.read(buffer, 256);
+    response.write(buffer, bytesRead);
+  }
+  f.close();
+  response.endBody();
+}
+
+void respondWith404(HttpRequest &request, HttpResponse &response) {
+  response.status(404, "NOT FOUND");
+  response.header("Content-Type", "text/plain");
+  response.beginBody();
+  response.printf("%s %s not found\n", request.getMethod().c_str(), request.getPath().c_str());
+}
+
+void defaultRequestHandler(HttpRequest &request, HttpResponse &response) {
   String path = request.getPath() == "/" ? "/index.html" : request.getPath();
   if (SPIFFS.exists(path)) {
-    byte buffer[256];
-    File f = SPIFFS.open(path, "r");
-    response.status(200, "FILE");
-    response.header("Content-Type", getMimeType(path));
-    response.header("Content-Size", String(f.size()));
-    response.beginBody();
-    while (f.position() < f.size()) {
-      int bytesRead = f.read(buffer, 256);
-      response.write(buffer, bytesRead);
-    }
-    f.close();
-    response.endBody();
+    respondWithFile(path, response);
   } else {
-    response.status(404, "NOT FOUND");
-    response.header("Content-Type", "text/plain");
-    response.beginBody();
-    response.printf("%s %s not found", request.getMethod().c_str(), request.getPath().c_str());
+    respondWith404(request, response);
   }
 }
 
@@ -47,7 +55,7 @@ void HttpServer::run() {
   WiFiClient client = server->available();
   if (client) {
     HttpRequest request(client);
-    PathHandler handler = handlers.get(request.getMethod() + " " + request.getPath(), defaultErrorHandler);
+    PathHandler handler = handlers.get(request.getMethod() + " " + request.getPath(), defaultRequestHandler);
     HttpResponse response(client);
     handler(request, response);
   }
